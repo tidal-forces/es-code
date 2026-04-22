77983595807fa2e4028ddad6b2f103dd71269520
tom gifford <giffordt@students.cs.ubc.ca>
2026-04-22 02:01:47 -0700

    added TODOs, moved helpers, added inline, const

--- ES5Encoder.cpp
+++ ES5Encoder.cpp
@@ -22,68 +22,11 @@ void ES5Encoder_Ctor(ES5Encoder* unit) {
     // if you don't do this, downstream ugens might access garbage memory in their Ctor functions.
     ES5Encoder_next(unit, 1);
 }
-
-
-// the calculation function can have any name, but this is conventional. the first argument must be "unit."
-// this function is called every control period (64 samples is typical)
-// Don't change the names of the arguments, or the helper macros won't work.
-void ES5Encoder_next(ES5Encoder* unit, int inNumSamples) {
-
-    // IN and OUT are helper macros that return audio-rate input and output buffers. These are known as "wire buffers."
-
-    // scsynth saves memory by aliasing wire buffers. In this case, "out" and "left" are the same. You should either
-    // be mindful of this behavior or turn it off in the PluginLoad section.
-    float *in1 = IN(0); // first header (ES-5 main panel)
-    float *in2 = IN(1); // second header
-    float *in3 = IN(2); // third header
-    float *in4 = IN(3); // fourth header
-    float *in5 = IN(4); // fifth header
-    float *in6 = IN(5); // sixth header
-
-    float *out7 = OUT(0); //  "left" output, ie ch7, drives headers 1,2,3
-    float *out8 = OUT(1); //  "right" output, ie ch8, drives headers 4,5,6
-
-    // Loop through samples and do the computation. *for* loop given in template
-    // ***pd version uses a *while* loop
-    // while (n--) & uses pointer arithmetic (*in1++ and *dstpL++)
-    for (int i = 0; i < inNumSamples; i++) {
-
-        //Clamp: ensure the incoming signals are kept strictly between 0 and 255 (an 8-bit byte) 
-        //and convert them to integers. ***pd:
-        /*  UInt32 out1 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in1++ ) ) );
-		 *  UInt32 out2 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in2++ ) ) );
-		 *  UInt32 out3 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in3++ ) ) );
-		 *  UInt32 out4 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in4++ ) ) );
-		 *  UInt32 out5 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in5++ ) ) );
-		 *  UInt32 out6 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in6++ ) ) ); */
-        //this ugen will use a clampByte helper
-
-
-        //Bit Packing: create the 24-bit word (N.B. 3 for each of ch7/left and ch8/right)
-        //Input 1 is shifted left by 16 bits (filling the top 8 bits).
-        //Input 2 is shifted left by 8 bits (filling the middle 8 bits).
-        //Input 3 stays where it is (filling the bottom 8 bits).
-        //***pd:
-		/*  SInt32 bitsL = ( out1 << 16 ) | ( out2 << 8 ) | out3;
-		 *  SInt32 bitsR = ( out4 << 16 ) | ( out5 << 8 ) | out6; */
-
-
-        //DSP Loop: process audio in "blocks" (arrays of samples) rather than one sample at a time
-        // ***pd uses pointer arithmetic (*in1++ / *dstpL++) moving pointer forward in memory w/ while loop
-        /*  float floatL = ES_BITSTOFLOAT( bitsL );
-		 *  float floatR = ES_BITSTOFLOAT( bitsR );
-
-		 *  *dstpL++ = floatL;
-		 *  *dstpR++ = floatR; */
-
-
-        
-    }
-}
+//**********_HELPERS_*************************************************************************************
 
 //clampByte helper: ensure the incoming signals are kept strictly between 0 and 255 ... etc... 
-uint32_t clampByte(float x) {
-    //STUB
+static inline uint32_t clampByte(float x) {
+    //TODO: STUB
 }
 
 //bit-to-float conversion helper: trick audio interface DAC into outputting the exact 24-bit digital word
@@ -95,9 +38,69 @@ uint32_t clampByte(float x) {
 
  *  #define ES_BITSTOFLOAT( bits )	\
  *  	( ( bits & 0x800000 ) ? ( (((float)(0xffffff&(-(SInt32)(bits)))) / btf_n_factor ) ) : ( ((float)(bits)) / btf_p_factor ) ) */
-float bitsToFloat24(uint32_t bits) {
-    //STUB
+static inline float bitsToFloat24(uint32_t bits) {
+    //TODO: STUB
 }
+
+//**********_CALCULATION_FXN_*************************************************************************************
+
+// the calculation function can have any name, but this is conventional. the first argument must be "unit."
+// this function is called every control period (64 samples is typical)
+// Don't change the names of the arguments, or the helper macros won't work.
+void ES5Encoder_next(ES5Encoder* unit, int inNumSamples) {
+
+    // IN and OUT are helper macros that return audio-rate input and output buffers. These are known as "wire buffers."
+
+    // scsynth saves memory by aliasing wire buffers. In this case, "out" and "left" are the same. You should either
+    // be mindful of this behavior or turn it off in the PluginLoad section.
+    const float *in1 = IN(0); // first header (ES-5 main panel)
+    const float *in2 = IN(1); // second header
+    const float *in3 = IN(2); // third header
+    const float *in4 = IN(3); // fourth header
+    const float *in5 = IN(4); // fifth header
+    const float *in6 = IN(5); // sixth header
+
+    float *out7 = OUT(0); //  "left" output, ie ch7, drives headers 1,2,3
+    float *out8 = OUT(1); //  "right" output, ie ch8, drives headers 4,5,6
+
+    // Loop through samples and do the computation. *for* loop given in template
+    // ***pd version uses a *while* loop
+    //      while (n--) & uses pointer arithmetic (*in1++ and *dstpL++)
+    for (int i = 0; i < inNumSamples; i++) {
+
+        //Clamp: ensure the incoming signals are kept strictly between 0 and 255 (an 8-bit byte) 
+        //and convert them to integers. ***pd:
+        /*  UInt32 out1 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in1++ ) ) );
+		 *  UInt32 out2 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in2++ ) ) );
+		 *  UInt32 out3 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in3++ ) ) );
+		 *  UInt32 out4 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in4++ ) ) );
+		 *  UInt32 out5 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in5++ ) ) );
+		 *  UInt32 out6 = (UInt32)( std::max( 0.0f, std::min( 255.0f, *in6++ ) ) ); */
+        //this ugen will use a clampByte helper
+    //TODO: STUB
+
+        //Bit Packing: create the 24-bit word (N.B. 3 for each of ch7/left and ch8/right)
+        //Input 1 is shifted left by 16 bits (filling the top 8 bits).
+        //Input 2 is shifted left by 8 bits (filling the middle 8 bits).
+        //Input 3 stays where it is (filling the bottom 8 bits).
+        //***pd:
+		/*  SInt32 bitsL = ( out1 << 16 ) | ( out2 << 8 ) | out3;
+		 *  SInt32 bitsR = ( out4 << 16 ) | ( out5 << 8 ) | out6; */
+    //TODO: STUB
+
+        //DSP Loop: process audio in "blocks" (arrays of samples) rather than one sample at a time
+        // ***pd uses pointer arithmetic (*in1++ / *dstpL++) moving pointer forward in memory w/ while loop
+        /*  float floatL = ES_BITSTOFLOAT( bitsL );
+		 *  float floatR = ES_BITSTOFLOAT( bitsR );
+		 *  *dstpL++ = floatL;
+		 *  *dstpR++ = floatR; */
+    //TODO: STUB
+
+        
+    }
+}
+
+//**********_FOR_UGEN_*************************************************************************************
 
 // the entry point is called by the host when the plug-in is loaded
 PluginLoad(ESPlugins) {
