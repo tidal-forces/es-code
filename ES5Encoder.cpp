435ec4f542a4d36213f32be8bbffb9b668449e0b
tom gifford <giffordt@students.cs.ubc.ca>
2026-04-22 01:02:01 -0700

    setting up templates

--- ES5Encoder.cpp
+++ ES5Encoder.cpp
@@ -4,30 +4,30 @@
 static InterfaceTable *ft;
 
 // declare struct to hold unit generator state
-struct abc : public Unit {
+struct ES5Encoder : public Unit {
     // declare state variables here.
 };
 
 //function declarations 
-static void abc_next(abc* unit, int inNumSamples);
-static void abc_Ctor(abc* unit);
+static void ES5Encoder_next(ES5Encoder* unit, int inNumSamples);
+static void ES5Encoder_Ctor(ES5Encoder* unit);
 
 // the constructor function is called when a Synth containing this ugen is played.
 // it MUST be named "PluginName_Ctor", and the argument must be "unit."
-static void abc_Ctor(abc* unit) {
+static void ES5Encoder_Ctor(ES5Encoder* unit) {
     // initialize state variables here.
     // set a calculation function. for now, we only have one calculation function.
-    SETCALC(abc_next);
+    SETCALC(ES5Encoder_next);
     // calculate one sample of output.
     // if you don't do this, downstream ugens might access garbage memory in their Ctor functions.
-    abc_next(unit, 1);
+    ES5Encoder_next(unit, 1);
 }
 
 
 // the calculation function can have any name, but this is conventional. the first argument must be "unit."
 // this function is called every control period (64 samples is typical)
 // Don't change the names of the arguments, or the helper macros won't work.
-void abc_next(abc* unit, int inNumSamples) {
+void ES5Encoder_next(ES5Encoder* unit, int inNumSamples) {
 
     // IN and OUT are helper macros that return audio-rate input and output buffers. These are known as "wire buffers."
     // In old ugens you'll see ZIN and ZOUT, which are not recommended.
@@ -47,9 +47,9 @@ void abc_next(abc* unit, int inNumSamples) {
 
 
 // the entry point is called by the host when the plug-in is loaded
-PluginLoad(xyz) {
+PluginLoad(ESPlugins) {
     // InterfaceTable *inTable implicitly given as argument to the load function
     ft = inTable; // store pointer to InterfaceTable
     // DefineSimpleUnit is one of four macros defining different kinds of ugens
-    DefineSimpleUnit(abc);
+    DefineSimpleUnit(ES5Encoder);
 }
