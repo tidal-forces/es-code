bc40b7a5c3e875cad46cb32aadb3dbffa2e8ecc9
tom gifford <giffordt@students.cs.ubc.ca>
2026-04-25 23:41:27 -0700

    a bare bones

--- compile_me_USE_ME_/ES5Encoder.cpp
+++ compile_me_USE_ME_/ES5Encoder.cpp
@@ -23,8 +23,8 @@ static void ES5Encoder_Ctor(ES5Encoder* unit) {
     // calculate one sample of output
     //**because still crashing? ES5Encoder_next(unit, 1);
     //MultiOutUGen  output buffer state unsafe??
-    OUT0(0) = 0.f;
-    OUT0(1) = 0.f;
+    /* OUT0(0) = 0.f;
+    OUT0(1) = 0.f; */
 }
 //**********_HELPERS_********************************************************************************
 
@@ -63,7 +63,7 @@ static inline float bitsToFloat24(uint32_t bits) {
 
 // this function is called every control period 
 static void ES5Encoder_next(ES5Encoder* unit, int inNumSamples) {
-    //because crashing?
+    //because crashing? TEMP
     float *es3ch7 = OUT(0);
     float *es3ch8 = OUT(1);
 
@@ -88,7 +88,7 @@ static void ES5Encoder_next(ES5Encoder* unit, int inNumSamples) {
         uint32_t h4 = clampByte(es5header4[i]);
         uint32_t h5 = clampByte(es5header5[i]);
         uint32_t h6 = clampByte(es5header6[i]); */
-        uint32_t h1 = clampByte(readInput(unit, 0, i));
+        /* uint32_t h1 = clampByte(readInput(unit, 0, i));
         uint32_t h2 = clampByte(readInput(unit, 1, i));
         uint32_t h3 = clampByte(readInput(unit, 2, i));
         uint32_t h4 = clampByte(readInput(unit, 3, i));
@@ -102,7 +102,10 @@ static void ES5Encoder_next(ES5Encoder* unit, int inNumSamples) {
         //DSP Loop: process audio in "blocks" (arrays of samples) rather than one sample at a time
         //Bits to float: Interpret the 24-bit word as signed 24-bit PCM and normalize to [-1, 1).
         es3ch7[i] = bitsToFloat24( bitsL );
-		es3ch8[i] = bitsToFloat24( bitsR );
+		es3ch8[i] = bitsToFloat24( bitsR ); */
+        //temp
+        es3ch7[i] = 0.f;
+        es3ch8[i] = 0.f;
     }
 }
 
